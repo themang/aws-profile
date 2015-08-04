8f8b82134886136b01391b5da1e86a1d2eeb120d
Josh Taylor <joshrtay@gmail.com>
2015-08-03 18:46:38 -0700

    update api

--- test/index.js
+++ test/index.js
@@ -7,23 +7,20 @@ describe('aws-profile', function () {
   it('should add and then delete test profile', function() {
     var profile = 'test-profile'
     var accessKeyId = "foobarfoobar"
-    awsProfile.add(profile, {
-      aws_access_key_id: accessKeyId,
-      aws_secret_access_key: "foobarfobar"
-    }, {
+
+    var config = {
+      accessKeyId: accessKeyId,
+      secretAccessKey: "foobarfobar",
       region: 'us-west-2',
       output: 'json'
-    })
+    }
+
+    awsProfile.add(profile, config)
     var cred = new aws.SharedIniFileCredentials({profile: profile})
     assert(cred.accessKeyId, accessKeyId)
 
-    var config = awsProfile.get(profile)
-    assert.deepEqual(config, {
-      aws_access_key_id: accessKeyId,
-      aws_secret_access_key: "foobarfobar",
-      region: 'us-west-2',
-      output: 'json'
-    })
+    var config2 = awsProfile.get(profile)
+    assert.deepEqual(config2, config)
 
     awsProfile.delete(profile)
     var cred = new aws.SharedIniFileCredentials({profile: profile})
