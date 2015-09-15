4a9a9ee944ce76e716a3c55571ad02421e8dd22f
Josh Taylor <joshrtay@gmail.com>
2015-09-15 14:50:15 -0700

    change api

--- test/index.js
+++ test/index.js
@@ -1,8 +1,17 @@
 var awsProfile = require('..')
 var assert = require('assert')
 var aws = require('aws-sdk')
+var mkdirp = require('mkdirp')
+var path = require('path')
+var rmdir = require('rmdir')
+
+var awsPath = path.join(__dirname, '.aws')
 
 describe('aws-profile', function () {
+
+  before(function () {
+    awsProfile.setAWSDir(awsPath)
+  })
 
   it('should add and then delete test profile', function() {
     var profile = 'test-profile'
@@ -11,8 +20,7 @@ describe('aws-profile', function () {
     var config = {
       accessKeyId: accessKeyId,
       secretAccessKey: "foobarfobar",
-      region: 'us-west-2',
-      output: 'json'
+      region: 'us-west-2'
     }
 
     awsProfile.add(profile, config)
@@ -22,12 +30,23 @@ describe('aws-profile', function () {
     var config2 = awsProfile.get(profile)
     assert.deepEqual(config2, config)
 
-    awsProfile.load(profile)
-    assert(aws.config.region === config.region)
+    awsProfile.login(profile)
+    var config3 = awsProfile.get()
+    assert.deepEqual(config3, config)
+
 
     awsProfile.delete(profile)
-    var cred = new aws.SharedIniFileCredentials({profile: profile})
-    assert(cred.accessKeyId === undefined)
+    awsProfile.delete('default')
+    var config4 = awsProfile.get()
+    assert.deepEqual(config4, {})
+    var config5 = awsProfile.get(profile)
+    assert.deepEqual(config5, {})
+  })
+
+  after(function (done) {
+    rmdir(awsPath, function () {
+      done()
+    })
   })
 
 })
